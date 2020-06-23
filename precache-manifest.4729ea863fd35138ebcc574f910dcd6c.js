01c38a860092da6dde5978c547837c55a62b1fa5
username <qkrdydals327@gmail.com>
2020-06-23 16:18:11 +0900

    Updates

---
+++ precache-manifest.4729ea863fd35138ebcc574f910dcd6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fb519c89a0a6ebab84a509126ec5e2",
+    "url": "/DragonSky2357/movie_app_2020/index.html"
+  },
+  {
+    "revision": "3558ce4b8d9f842c2f27",
+    "url": "/DragonSky2357/movie_app_2020/static/css/main.d7bd83a0.chunk.css"
+  },
+  {
+    "revision": "a5d551576243306f81d7",
+    "url": "/DragonSky2357/movie_app_2020/static/js/2.1c2ec9d4.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/DragonSky2357/movie_app_2020/static/js/2.1c2ec9d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3558ce4b8d9f842c2f27",
+    "url": "/DragonSky2357/movie_app_2020/static/js/main.ff3217ae.chunk.js"
+  },
+  {
+    "revision": "7e8126b7b349d92618a3",
+    "url": "/DragonSky2357/movie_app_2020/static/js/runtime-main.9cf2cf33.js"
+  }
+]);
